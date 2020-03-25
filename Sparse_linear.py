4619d540d7374f729ad7e233ba62de4e4628afec
Xie <zxie3@sbbindplp001.uthouston.edu>
2020-03-25 18:12:11 -0500

    updated r2 calculation

--- Sparse_linear.py
+++ Sparse_linear.py
@@ -4,22 +4,6 @@ from collections import Counter
 
 
 class SparseLinear(torch.nn.Module):
-    def __init__(self, num_gene, dim_rep, row_ind):
-        # ind is of shape (num_gene, num_nonzero)
-        super().__init__()
-        self.dim_x = num_gene
-        self.dim_y = dim_rep
-        self.ind = torch.nn.Parameter(torch.tensor(row_ind).unsqueeze(0), requires_grad=False)
-        self.nnz = self.ind.shape[-1]
-        self.w = torch.nn.Parameter(torch.randn(self.dim_x, self.dim_y, self.nnz)/np.sqrt(self.nnz))
-
-    def forward(self, x):
-        # x is of shape (batch, num_cpg)
-        gathered = x.unsqueeze(2).expand(-1, -1, self.nnz).gather(1, self.ind.expand(x.shape[0],-1, -1))
-        return torch.einsum("gvc, bgc->bgv", self.w, gathered)
-
-
-class SparseLinear2(torch.nn.Module):
     def __init__(self, num_gene, dim_rep, indices):
         # ind is a list of list or tuple of coordinate (i, j) that marks the nonzero entries in the weight
         super().__init__()
@@ -30,6 +14,7 @@ class SparseLinear2(torch.nn.Module):
         self.dim_x = len(indices)
         self.dim_y = dim_rep
         self.w = torch.nn.Parameter(torch.randn(self.dim_x, self.dim_y).float()/np.sqrt(median_nnz))
+        self.b = torch.nn.Parameter(torch.zeros(1, num_gene, dim_rep))
 
     def forward(self, x):
         # x is of shape (batch, num_cpg)
@@ -38,12 +23,10 @@ class SparseLinear2(torch.nn.Module):
         product = torch.einsum("xv, bx -> bxv", self.w, selected)
         return torch.zeros(num_batch, self.num_gene,
                            self.dim_y).to(x.device).scatter_add_(1, self.ind[:, :1].unsqueeze(0).expand(num_batch, -1, self.dim_y),
-                                                    product)
+                                                    product) + self.b
 
 
-# test
-# s = SparseLinear(3, 5, 2, [[0],[1],[2]])
-# s(torch.randn(10, 5)).sum().backward()
-# s = SparseLinear2(3, 2, [[0, 0],[1, 1],[2, 1],[2, 2]])
+
+# s = SparseLinear(3, 2, [[0, 0],[1, 1],[2, 1],[2, 2]])
 # s(torch.randn(10, 5)).sum().backward()
 # print(s.w.grad)
--- train.py
+++ train.py
@@ -1,14 +1,19 @@
-from Sparse_linear import SparseLinear2
+from Sparse_linear import SparseLinear
 from torch.utils.data import DataLoader, SubsetRandomSampler
 from dataLoader import csvDataset_mem
 import sys
 import torch
-from torch.optim import SGD
+from torch.optim import Adam as optim
 import numpy as np
+from os import path
+import pickle as pkl
 
 
-def get_indices(cpg_file, gene_exp_file, meta_file):
-    with open(cpg_file, 'r') as f, open(gene_exp_file, 'r') as g, open(meta_file, 'r') as meta:
+def get_indices(cpg_file, gene_exp_file, meta_file, index_save_path):
+    if path.exists(index_save_path):
+        with open(index_save_path, 'rb') as f:
+            return pkl.load(f)
+    with open(cpg_file, 'r') as f, open(gene_exp_file, 'r') as g, open(meta_file, 'r') as meta, open(index_save_path, 'wb') as index_save_path:
         cpg_sites = list(map(lambda x: x[1:-1], f.readline().strip().split(',')[1:]))
         cpg_site_dict = dict(zip(cpg_sites, range(len(cpg_sites))))
         gene_exp = list(map(lambda x: x[1:-1], g.readline().strip().split(',')[1:]))
@@ -16,30 +21,42 @@ def get_indices(cpg_file, gene_exp_file, meta_file):
         meta.readline()
         indices = []
         for line in meta:
-           gene, cpgs = line.strip().split(',')
-           cpgs = cpgs.split(':')
-           gene_idx = gene_exp_dict.get(gene, None)
-           if gene_idx is None:
-               continue
-           for cpg in cpgs:
-               cpg_idx = cpg_site_dict.get(cpg, None)
-               if cpg_idx is None:
-                   continue
-               indices.append([gene_idx, cpg_idx])
+            gene, cpgs = line.strip().split(',')
+            cpgs = cpgs.split(':')
+            gene_idx = gene_exp_dict.get(gene, None)
+            if gene_idx is None:
+                continue
+            for cpg in cpgs:
+                cpg_idx = cpg_site_dict.get(cpg, None)
+                if cpg_idx is None:
+                    continue
+                indices.append([gene_idx, cpg_idx])
+        pkl.dump(indices, index_save_path)
     return indices
 
 
-def variance(dataset, idxs):
-    cnt = 0
-    base = dataset[idxs[0]][1]
-    cum = torch.zeros_like(base)
-    cumsq = torch.zeros_like(base)
-    for i in idxs[1:]:
-        cnt += 1
-        expr = dataset[i][1]
-        cum += (expr - base)
-        cumsq += (expr - base) ** 2
-    return cumsq**2/cnt - (cum/cnt)**2
+class running_mean_var:
+    def __init__(self, eps=1e-8):
+        self.sum = 0
+        self.sumsq = 0
+        self.cnt = 0
+        self.eps = eps
+ 
+    def update(self, x):
+        with torch.no_grad():
+            self.sum += x.sum(0)
+            self.sumsq += (x**2).sum(0)
+            self.cnt += x.shape[0]
+
+    def get_mean_var(self):
+        mean = self.sum/self.cnt
+        return mean, (self.sumsq - mean*self.sum)/(self.cnt-1)+self.eps
+        
+
+
+def expr_mean_var(dataset, idxs, device):
+    expr = dataset.gene[idxs]
+    return expr.mean(0).to(device), expr.var(0).to(device)
 
 
 device = "cuda"
@@ -50,36 +67,50 @@ BATCHSIZE = int(sys.argv[4])
 EPOCH = int(sys.argv[5])
 save_path = sys.argv[6].strip('/')
 npz_path = sys.argv[7]
-
-indices = get_indices(cpg_file, gene_file, meta_file)
-model = SparseLinear2(17113, 1, indices).to(device)
-optimizer = SGD(model.parameters(), lr=1e-3, momentum=0.9)
+index_save_path = sys.argv[8]
+indices = get_indices(cpg_file, gene_file, meta_file, index_save_path)
+model = SparseLinear(17113, 1, indices).to(device)
+optimizer = optim(model.parameters(), lr=1e-3)
 dataset = csvDataset_mem([cpg_file, gene_file], npz_path)
 split = int(len(dataset) * 0.8)
 train_idx = list(range(split))
 test_idx = list(range(split, len(dataset)))
 trainLoader = DataLoader(dataset, batch_size=BATCHSIZE, sampler=SubsetRandomSampler(train_idx))
 testLoader = DataLoader(dataset, batch_size=BATCHSIZE, sampler=SubsetRandomSampler(test_idx))
-Loss = lambda x, y, w: ((x - y)**2 / w).sum()/x.shape[1]
+Loss = lambda x, y, w: ((x - y)**2 / w).mean()
 best_loss = np.inf
-train_var = variance(dataset, train_idx).to(device)
-test_var = variance(dataset, test_idx).to(device)
-
+train_mean, train_var = expr_mean_var(dataset, train_idx, device)
+test_mean, test_var = expr_mean_var(dataset, test_idx, device)
 for i in range(EPOCH):
+    out_estimator = running_mean_var()
+    running_product = 0
     for cpg, gene_exp in trainLoader:
         cpg = cpg.to(device)
         gene_exp = gene_exp.to(device)
         optimizer.zero_grad()
-        Loss(model(cpg).squeeze(2), gene_exp, train_var).backward()
+        out = model(cpg).squeeze(2)
+        out_estimator.update(out)
+        with torch.no_grad():
+            running_product += (out*gene_exp).sum(0)
+        loss = Loss(out, gene_exp, train_var)
+        loss.backward()
         optimizer.step()
+    out_mean, out_var = out_estimator.get_mean_var()
+    train_r2 = ((running_product/len(train_idx) - out_mean*train_mean)**2/out_var / train_var).mean()
+    print("epoch {} training r2: {}".format(i+1, train_r2))
+    running_product = 0
+    out_estimator = running_mean_var()
+    best_test_r2 = -np.inf
     for cpg, gene_exp in testLoader:
-        running_loss = 0
         with torch.no_grad():
             cpg = cpg.to(device)
             gene_exp = gene_exp.to(device)
-            running_loss += Loss(model(cpg).squeeze(2), gene_exp, test_var)
-    test_loss = running_loss/len(test_idx)
-    if test_loss < best_loss:
-        best_loss = test_loss
+            out = model(cpg).squeeze(2)
+            out_estimator.update(out)
+            running_product += (out*gene_exp).sum(0)
+    out_mean, out_var = out_estimator.get_mean_var()
+    test_r2 = ((running_product/len(test_idx) - out_mean*test_mean)**2/out_var / test_var).mean()
+    if test_r2 > best_test_r2:
+        best_test_r2 = test_r2
         torch.save(model, '{}/best_model.pth'.format(save_path))
-    print("epoch {} testing r2: {}".format(i+1, 1 - running_loss/len(test_idx)))
+    print("epoch {} testing r2: {}".format(i+1, test_r2))
