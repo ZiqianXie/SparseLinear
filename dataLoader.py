f5b796bbfe774ea544578cbf79e1216b2d30d1a4
Xie <zxie3@sbbindplp001.uthouston.edu>
2020-03-25 14:24:55 -0500

    load all data into memory, using variance to weight the loss

--- dataLoader.py
+++ dataLoader.py
@@ -1,6 +1,8 @@
 from torch.utils.data import Dataset
 import pickle as pkl
 import numpy as  np
+import torch
+from os import path
 
 
 def precompute_tell(files, save_path):
@@ -37,7 +39,36 @@ class csvDataset(Dataset):
         for i, f in enumerate(self.fs):
             f.seek(self.pos[i][idx])
             line = f.readline()
-            outs.append(np.fromstring(line[line.index(',')+1:-1], sep=','))
+            outs.append(torch.from_numpy(np.fromstring(line[line.index(',')+1:-1], sep=',')).float())
         return outs
+    
+
+class csvDataset_mem(Dataset):
+    def __init__(self, csv_files, npz_path):
+        if not path.exists(npz_path):
+            cpg, gene = csv_files
+            self.cpg = np.empty((874, 395505))
+            self.gene = np.empty((874, 17113))
+            with open(cpg, 'r') as f, open(gene, 'r') as g:
+                f.readline()
+                g.readline()
+                for i, (line1, line2) in enumerate(zip(f, g)):
+                    self.cpg[i] = np.fromstring(line1[line1.index(',')+1:-1], sep=',')
+                    self.gene[i] = np.fromstring(line2[line2.index(',')+1:-1], sep=',')
+            np.savez(npz_path, cpg=self.cpg, gene=self.gene)
+        else:
+            data = np.load(npz_path)
+            self.cpg = data['cpg']
+            self.gene = data['gene']
+        self.cpg = torch.from_numpy(self.cpg).float()
+        self.gene = torch.from_numpy(self.gene).float()
+
+    def __len__(self):
+        return 874
+
+    def __getitem__(self, idx):
+        return [self.cpg[idx], self.gene[idx]]
+                
+                
 
 
--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 from Sparse_linear import SparseLinear2
 from torch.utils.data import DataLoader, SubsetRandomSampler
-from dataLoader import csvDataset
+from dataLoader import csvDataset_mem
 import sys
 import torch
-from torch.nn import MSELoss
 from torch.optim import SGD
+import numpy as np
 
 
 def get_indices(cpg_file, gene_exp_file, meta_file):
@@ -29,38 +29,57 @@ def get_indices(cpg_file, gene_exp_file, meta_file):
     return indices
 
 
+def variance(dataset, idxs):
+    cnt = 0
+    base = dataset[idxs[0]][1]
+    cum = torch.zeros_like(base)
+    cumsq = torch.zeros_like(base)
+    for i in idxs[1:]:
+        cnt += 1
+        expr = dataset[i][1]
+        cum += (expr - base)
+        cumsq += (expr - base) ** 2
+    return cumsq**2/cnt - (cum/cnt)**2
+
+
 device = "cuda"
 gene_file = sys.argv[1]
 cpg_file = sys.argv[2]
 meta_file = sys.argv[3]
 BATCHSIZE = int(sys.argv[4])
 EPOCH = int(sys.argv[5])
-if len(sys.argv) > 6:
-    pickled_pos = sys.argv[6]
-else:
-    pickled_pos = None
+save_path = sys.argv[6].strip('/')
+npz_path = sys.argv[7]
 
 indices = get_indices(cpg_file, gene_file, meta_file)
 model = SparseLinear2(17113, 1, indices).to(device)
 optimizer = SGD(model.parameters(), lr=1e-3, momentum=0.9)
-dataset = csvDataset([cpg_file, gene_file], pickled_pos)
+dataset = csvDataset_mem([cpg_file, gene_file], npz_path)
 split = int(len(dataset) * 0.8)
 train_idx = list(range(split))
 test_idx = list(range(split, len(dataset)))
 trainLoader = DataLoader(dataset, batch_size=BATCHSIZE, sampler=SubsetRandomSampler(train_idx))
 testLoader = DataLoader(dataset, batch_size=BATCHSIZE, sampler=SubsetRandomSampler(test_idx))
-Loss = MSELoss().to(device)
+Loss = lambda x, y, w: ((x - y)**2 / w).sum()/x.shape[1]
+best_loss = np.inf
+train_var = variance(dataset, train_idx).to(device)
+test_var = variance(dataset, test_idx).to(device)
+
 for i in range(EPOCH):
     for cpg, gene_exp in trainLoader:
-        cpg = torch.tensor(cpg).float().to(device)
-        gene_exp = torch.tensor(gene_exp).float().to(device)
+        cpg = cpg.to(device)
+        gene_exp = gene_exp.to(device)
         optimizer.zero_grad()
-        Loss(model(cpg).squeeze(2), gene_exp).backward()
+        Loss(model(cpg).squeeze(2), gene_exp, train_var).backward()
         optimizer.step()
     for cpg, gene_exp in testLoader:
         running_loss = 0
         with torch.no_grad():
-            cpg = torch.tensor(cpg).float().to(device)
-            gene_exp = torch.tensor(gene_exp).float().to(device)
-            running_loss += Loss(model(cpg).squeeze(2), gene_exp)
-    print("epoch {} testing r2: {}".format(i+1, running_loss/len(test_idx)))
+            cpg = cpg.to(device)
+            gene_exp = gene_exp.to(device)
+            running_loss += Loss(model(cpg).squeeze(2), gene_exp, test_var)
+    test_loss = running_loss/len(test_idx)
+    if test_loss < best_loss:
+        best_loss = test_loss
+        torch.save(model, '{}/best_model.pth'.format(save_path))
+    print("epoch {} testing r2: {}".format(i+1, 1 - running_loss/len(test_idx)))
